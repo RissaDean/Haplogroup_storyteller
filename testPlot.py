4e178736d88d797365cd058692f148fb41fbc6a3
Rissa <inf-48-2025@bioinf-biol302448.biol.lu.se>
2026-03-12 14:37:38 +0100

    somewhat functional graphing script

--- testPlot.py
+++ testPlot.py
@@ -5,23 +5,82 @@ Created on Wed Mar 11 12:12:30 2026
 
 @author: inf-48-2025
 """
-from plotly.offline import plot
-import plotly.graph_objects as go
-from igraph import Graph, EdgeSeq
+#import networkx as nx
+#import matplotlib.pyplot as plt
+#import numpy as np
+
+# %%
+# seed = 6
+
+# np.random.seed(seed)
+# usergroup = "U2c1"
+
+# nodes = []
+# edges = []
+# terminal_nodes = []
+# layers = range(1,(len(usergroup)))
+# G = nx.DiGraph()
+
+# # for i in layers:
+# #     nodes.append(i)
+
+# for index, letter in enumerate(usergroup):
+#     nodes.append(usergroup[0:index+1])
+#     if index > 0:
+#         #edges.append((index, usergroup[0:index+1]))
+#         edges.append((usergroup[0:index], usergroup[0:index+1]))
+
+        
+# root_node = usergroup
+
+    
+
+# G.add_nodes_from(nodes)   
+# G.add_edges_from(edges)
+
+
+
+# %%
+   
+
+# plt.figure(figsize=(8, 6))
+# pos = nx.spring_layout(G)
+# pos[root_node] = (-2,(1/len(layers)))
+# #write_dot(G,'test.dot')
+# nx.draw(G, pos, with_labels=True, node_size=3000, node_color='skyblue', font_size=10, font_weight='bold', edge_color='gray')
+# plt.gca().invert_yaxis()
+# plt.title('Pedigree Analysis Chart')
+# plt.show()
+
 # %%
 
+import igraph
+from igraph import Graph, EdgeSeq
+import plotly.graph_objects as go
+from plotly.offline import plot
 
-nr_vertices = 25
+usergroup = "U2c1"
+layers = len(usergroup)
+
+nr_vertices = (len(usergroup)+(len(usergroup)-1))
 v_label = list(map(str, range(nr_vertices)))
 G = Graph.Tree(nr_vertices, 2) # 2 stands for children number
-lay = G.layout('rt')
 
-position = {k: lay[k] for k in range(nr_vertices)}
-Y = [lay[k][1] for k in range(nr_vertices)]
-M = max(Y)
+position = []
+for k in range(0,layers):
+    position.append((((k - (layers/2))),((layers-k))))
+for k in range(0,layers):
+    if k > 0:
+       position.append((((k - (layers/2) + 0.5)),((layers-k+1)))) 
 
-es = EdgeSeq(G) # sequence of edges
-E = [e.tuple for e in G.es] # list of edges
+M = layers
+
+E = [] 
+for i in range(0,layers-1):
+    E.append((i,i+1))
+for i in range(0,layers):    
+    if i > 0:
+        E.append((i+layers-1,i))
 
 L = len(position)
 Xn = [position[k][0] for k in range(L)]
@@ -29,20 +88,44 @@ Yn = [2*M-position[k][1] for k in range(L)]
 Xe = []
 Ye = []
 for edge in E:
+    print(position[edge[0]][0], position[edge[1]][0])
+
     Xe+=[position[edge[0]][0],position[edge[1]][0], None]
     Ye+=[2*M-position[edge[0]][1],2*M-position[edge[1]][1], None]
 
-labels = v_label
-# %%
 
-# g = Graph()
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=Xn,
+                   y=Yn,
+                   mode='lines',
+                   line=dict(color='rgb(210,210,210)', width=1),
+                   hoverinfo='none'
+                   ))
+fig.add_trace(go.Scatter(x=Xn,
+                  y=Yn,
+                  mode='markers',
+                  name='bla',
+                  marker=dict(symbol='circle-dot',
+                                size=18,
+                                color='#6175c1',    #'#DB4551',
+                                line=dict(color='rgb(50,50,50)', width=1)
+                                ),
+                  opacity=0.8
+                  ))
 
-# g.add_vertices(3)
-# g.add_edges([(0,1), (1,2)])
+axis = dict(showline=False, # hide axis line, grid, ticklabels and  title
+            zeroline=False,
+            showgrid=False,
+            showticklabels=False,
+            )
 
-# g = Graph.Tree(8, 2)
-# layout = g.layout_reingold_tilford(root=[2])
-# plot(g, layout=layout)
-
-
-
+fig.update_layout(title= 'Tree with Reingold-Tilford Layout',
+              font_size=12,
+              showlegend=False,
+              xaxis=axis,
+              yaxis=axis,
+              margin=dict(l=40, r=40, b=85, t=100),
+              hovermode='closest',
+              plot_bgcolor='rgb(248,248,248)'
+              )
+plot(fig, auto_open=True)
